982f481e5cdd906c2f2d26244f851969a0d2598e
Paco Ortiz <pacoortiz16@hotmail.com>
2023-06-04 14:27:19 -0600

    feat: Protected routes patch and delete user

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -6,7 +6,7 @@ const passportJwt = require('../middleware/auth.middleware')
 router.get('/test', userServices.test)
 router.get('/users', userServices.getAllUsers)
 router.post('/users', userServices.postNewUser)
-router.patch('/users', passportJwt, userServices.patchUser)
-router.delete('/users', passportJwt, userServices.deleteUser)
+router.patch('/users/me', passportJwt, userServices.patchUser)
+router.delete('/users/me', passportJwt, userServices.deleteUser)
 
 module.exports = router
